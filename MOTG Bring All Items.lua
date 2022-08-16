3f68ce0b7037373b243e621f8d704a41642e0e4d
JayScripts <111326336+JayFancyScripts@users.noreply.github.com>
2022-08-16 12:59:00 -0400

    Add files via upload

---
+++ MOTG Bring All Items.lua
@@ -0,0 +1,20 @@
+local a = game:GetService("Workspace").Items:getDescendants()
+
+local b = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+for i,v in pairs(a) do
+    if v:IsA('Part') and v.Name == 'Base' then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+        wait(1)
+        game:GetService("ReplicatedStorage").Resources.Remotes.InteractItem:FireServer("Take",v.Parent)
+        wait(1)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = b
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+        wait(1)
+        game:GetService("ReplicatedStorage").Resources.Remotes.InteractItem:FireServer("Drop",v.Parent)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+        wait(1)
+    end
+end
